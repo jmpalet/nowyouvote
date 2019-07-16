21a1b8fc045128bb685ae692ff37c791dc182d21
Josep Palet <joseppaletc@gmail.com>
2019-07-16 19:14:13 +0200

    Remove bootstrap

--- src/main.js
+++ src/main.js
@@ -4,14 +4,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import FirebasePlugin from './plugins/Firebase'
-import BootstrapVue from 'bootstrap-vue'
 
-Vue.use(BootstrapVue)
 Vue.use(FirebasePlugin)
 Vue.config.productionTip = false
-
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   router,
